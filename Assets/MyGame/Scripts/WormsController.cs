2b9c657b233b5ae4fe057eadf5540cfbae2e3d27
aleksaradic <aleksaradic01@gmail.com>
2020-01-29 11:30:23 +0100

    Worm Controller Script, moving forwards and backwards, also jumping

---
+++ Assets/MyGame/Scripts/WormsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumpForce;
+    public float moveForce;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 force = new Vector3(0, jumpForce, 0);
+
+            rb.AddForce(force);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 force = new Vector3(-moveForce, 0, 0);
+
+            rb.AddForce(force);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 force = new Vector3(moveForce, 0, 0);
+
+            rb.AddForce(force);
+        }
+    }
+}
